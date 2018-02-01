47ca6d894ef101d7c714a678b62a97558d9ac858
Louis Mazza <louismazza@gmail.com>
2018-02-01 06:48:57 -0800

    remove localhost from config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/BlogPostDb'|| 'mongodb://lemazza:12r46a@ds121898.mlab.com:21898/bpostdb';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://lemazza:12r46a@ds121898.mlab.com:21898/bpostdb';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-BlogPostDb';
 exports.PORT = process.env.PORT || 8080;
